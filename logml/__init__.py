f5818c70a3e6d74a4468638cf5c4246b5a7279c0
Valentin Goldité <valentin.goldite@gmail.com>
2023-04-28 02:42:39 +0200

    🔖 Add __version__ attribute

--- logml/__init__.py
+++ logml/__init__.py
@@ -11,7 +11,14 @@ Copyright (C) 2023  Valentin Goldité
     DISTRIBUTION and PRIVATE USE as long as the original license is
     include as well as this copy right notice.
 """
-
 from logml.logger import Logger
 
-__all__ = ['Logger']
+try:
+    from logml._version import version as __version__
+    from logml._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
+
+__all__ = ['__version__', 'version_tuple', 'Logger']
