180a937995a92a039fdda522df9d6bf3757e8349
valentingol <valentin.goldite@gmail.com>
2025-01-13 14:36:51 +0100

    🔧 Add pause integration test

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,11 +1,18 @@
 echo "*********** Integration tests ***********"
 echo "*** logger ***"
 python tests/integration/inte_logger.py
+echo ""
 echo "*** regex ***"
 python tests/integration/inte_regex.py
+echo ""
 echo "*** tqdm ***"
 python tests/integration/inte_tqdm.py
+echo ""
 echo "*** 2 loggers ***"
 python tests/integration/inte_two_loggers.py
+echo ""
 echo "*** multi logs ***"
 python tests/integration/inte_multi_logs.py
+echo ""
+echo "*** pause/resume ***"
+python tests/integration/inte_pause.py
